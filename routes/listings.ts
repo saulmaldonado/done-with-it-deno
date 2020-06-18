7a6c498a3ff15ca6d416e221e0684857738de20c
Saul Maldonado <samaldonado4@gmail.com>
2020-06-18 03:52:50 -0500

    Create tests for listing routes
    Test endpoints for listing routes.

--- routes/listings.ts
+++ routes/listings.ts
@@ -1,9 +1,7 @@
 import { Router } from 'https://deno.land/x/oak/mod.ts';
-import { readJson } from 'https://deno.land/std/fs/read_json.ts';
-import { Listing } from '../schema.ts';
 import { getAllListings, getListingById } from '../controllers/listings.ts';
 
 export const listingRoutes = (router: Router, path: string) => {
-  router.get(`${path}/`, getAllListings);
+  router.get(`${path}`, getAllListings);
   router.get(`${path}/:id`, getListingById);
 };
---
+++ tests/listing.test.ts
@@ -0,0 +1,30 @@
+import { assertEquals, assert } from 'https://deno.land/std/testing/asserts.ts';
+import { readJson } from 'https://deno.land/std/fs/mod.ts';
+import { Listing } from '../schema.ts';
+
+const listings = (await readJson('./db.json')) as Listing[];
+const baseUrl = 'http://localhost:8000';
+
+Deno.test('/api/v1/listings should return all listings', async () => {
+  const result = await fetch(baseUrl + '/api/v1/listings/');
+
+  const resultListing = (await result.json()) as Listing[];
+
+  assertEquals(resultListing.length, listings.length);
+});
+
+Deno.test('/api/v1/listings/1 should return listing with an id of 1', async () => {
+  const result = await fetch(baseUrl + '/api/v1/listings/1');
+  const resultListing = (await result.json()) as Listing;
+
+  assert(result.ok);
+  assertEquals(resultListing.id, 1);
+});
+
+Deno.test('Invalid paths should fail', async () => {
+  const { ok, body } = await fetch(baseUrl + '/api/v1/listing');
+
+  assertEquals(ok, false);
+
+  await body?.cancel();
+});
