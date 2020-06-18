59c94236b134578e137860e947cacf056214e097
Saul Maldonado <samaldonado4@gmail.com>
2020-06-18 09:36:28 -0500

    Add tests for custom JWT methods

---
+++ tests/jwtAuth.test.ts
@@ -0,0 +1,36 @@
+import { assertEquals, assert } from 'https://deno.land/std/testing/asserts.ts';
+import { genToken, validateToken } from '../auth/jwtAuth.ts';
+import { makeJwt, setExpiration, Jose, Payload } from 'https://deno.land/x/djwt/create.ts';
+import { validateJwt } from 'https://deno.land/x/djwt/validate.ts';
+
+const jwtTest =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJkb25ld2l0aGl0IiwidXNlcmlkIjoxfQ.pt7uIVX6MKUiKu0Wht32Kn7Y-eTHSrSgZ9R_NcMX3uU';
+
+Deno.test('genToken should return the default key when no options are passed in', () => {
+  let token = genToken();
+
+  assertEquals(token, jwtTest);
+});
+
+Deno.test('genToken should return a valid JWT token', async () => {
+  const header: Jose = { alg: 'HS256', type: 'jwt' };
+  const payload = { iss: 'deno', userid: 0 };
+  const key = 'secretkey';
+
+  let token = genToken(header, payload, key);
+
+  let jwtToken = makeJwt({ header, payload, key });
+
+  assert((await validateJwt(token, key)).isValid);
+  assertEquals(token, jwtToken);
+});
+
+Deno.test('validate should return true on a valid token', async () => {
+  const header: Jose = { alg: 'HS256', type: 'jwt' };
+  const payload = { iss: 'deno', userid: 0 };
+  const key = 'secretkey';
+
+  let jwtToken = makeJwt({ header, payload, key });
+
+  assert(await validateToken(jwtToken, key));
+});
