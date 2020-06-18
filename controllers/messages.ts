2ef9e61de75ab38a4b56d3e71d4bdebb6f4e24f0
Saul Maldonado <samaldonado4@gmail.com>
2020-06-18 06:29:04 -0500

    Add controller for getting user's messages
    Add controller for filtering through db messages and get messages for
    user. Add mock authentication as a placeholder

---
+++ controllers/messages.ts
@@ -0,0 +1,22 @@
+import { messages } from '../index.ts';
+import { RouterContext } from 'https://deno.land/x/oak/mod.ts';
+
+const getAllMessagesForUser = async ({ request, throw: throwError, response }: RouterContext) => {
+  let token = request.headers.get('Authorization');
+  if (!token) {
+    throwError(401, 'You must be logged in.');
+  } else if (token !== '1234') {
+    throwError(403, 'You are not authorized to request these messages');
+  } else {
+    const body: { userId: number } = (await request.body()).value;
+    const id = body.userId;
+
+    const userMessages = messages.filter(
+      (message) => message.fromUserId === id || message.toUserId === id
+    );
+
+    response.body = userMessages;
+  }
+};
+
+export { getAllMessagesForUser };
--- routes/messages.ts
+++ routes/messages.ts
@@ -1,7 +1,8 @@
 import { Router } from 'https://deno.land/x/oak/mod.ts';
 import { RouterContext } from 'https://deno.land/x/oak/mod.ts';
+import { getAllMessagesForUser } from '../controllers/messages.ts';
 
 export default (router: Router, path: string) => {
   router.get(`${path}`, () => console.log('messages works'));
-  router.get(`${path}/:id`, (ctx: RouterContext) => console.log(`messages id ${ctx.params.id} `));
+  router.get(`${path}/:id`, getAllMessagesForUser);
 };
