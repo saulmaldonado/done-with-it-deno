db1fdba5e54dfd7d2069b026b0757b511e302a7f
Saul Maldonado <samaldonado4@gmail.com>
2020-06-25 07:05:22 -0500

    Add middlware for static path. Added hostname property to app
    Add paths to static content for accessing images. Add new hostname
    property to use local IP instead of localhost.

--- index.ts
+++ index.ts
@@ -3,8 +3,10 @@ import { notFound } from './middleware/notFound.ts';
 import { router } from './routes/root.ts';
 import { initializeImageMagick } from 'https://deno.land/x/deno_imagemagick/mod.ts';
 import { config } from './environment.dev.ts';
+import { serverStatic } from './middleware/static.ts';
 
 const port: number = config.PORT_NUMBER;
+const hostname: string = config.HOST_NAME;
 
 const app = new Application();
 await initializeImageMagick(); // image resizer
@@ -17,7 +19,8 @@ app.addEventListener('listen', ({ hostname, port, secure }) => {
 // Routes
 app.use(router.routes());
 app.use(router.allowedMethods());
+app.use(serverStatic);
 
 //Catch invalid path
 app.use(notFound);
-await app.listen({ port });
+await app.listen({ hostname, port });
---
+++ middleware/static.ts
@@ -0,0 +1,18 @@
+import { Context, send, Middleware } from 'https://deno.land/x/oak/mod.ts';
+
+export const serverStatic: Middleware<any, Context> = async (ctx, next) => {
+  if (ctx.request.url.pathname.startsWith('/assets')) {
+    try {
+      const result = await send(ctx, ctx.request.url.pathname, {
+        root: `${Deno.cwd().replace(/\\/g, '/')}/public`,
+      });
+      if (!result) {
+        throw Error;
+      }
+    } catch {
+      ctx.throw(400, 'Invalid static path');
+    }
+  } else {
+    await next();
+  }
+};
